59297c8687a9f01a911fca890c3d6a32b08f1cf0
shalom-nganga <minnomacharia@gmail.com>
2023-11-12 23:20:25 +0300

    added content in the app.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import TransactionList from './TransactionList';
+import TransactionForm from './TransactionForm';
 
-function App() {
+const App = () => {
+  // Declare the state variables for transactions and filteredTransactions
+  const [transactions, setTransactions] = useState([]);
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // Your code for fetching the data and setting the transactions state can go here.
+  // Make sure to fetch the data and update the transactions state accordingly.
+  useEffect(() => {
+    // Fetch the data from the file
+    fetch('http://localhost:8000/transactions') // Replace the URL with the correct path to your db.json file
+      .then((response) => response.json())
+      .then((data) => setTransactions(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  // Define the handleSearch function
+  const handleSearch = () => {
+    const filtered = transactions.filter(transaction =>
+      transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredTransactions(filtered);
+  };
+
+  // Update the filtered transactions when the searchTerm changes
+  useEffect(() => {
+    handleSearch();
+  }, [handleSearch, searchTerm, transactions]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Transactions</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Search by description..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      </div>
+
+      {/* Display the fetched data (filtered transactions) */}
+      {filteredTransactions && <TransactionList transaction={filteredTransactions} />}
+
+      {/* Display the form after the fetched data */}
+      <div>
+        <h1>Add Transaction</h1>
+        <TransactionForm />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
